ba72649965e7220e4cf39ecf77a859b4076321f1
aliyaghobi <aliymix07@gmail.com>
2022-04-29 22:47:52 +0430

    last commit

---
+++ lib/widgets/get_last_message.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GetLastMessage extends StatefulWidget {
+  const GetLastMessage({Key? key}) : super(key: key);
+
+  @override
+  State<GetLastMessage> createState() => _GetLastMessageState();
+}
+
+class _GetLastMessageState extends State<GetLastMessage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
